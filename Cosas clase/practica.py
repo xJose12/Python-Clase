880eba86f0cd439f390c7e644128dc9c929abb84
xJose12 <joseantonio022002@gmail.com>
2023-11-14 17:40:55 +0100

    Update practica.py

--- Cosas clase/practica.py
+++ Cosas clase/practica.py
@@ -4,12 +4,14 @@ print("El coste de las manzanas es:" + str(coste_manzana))
 manzanas = input("cuantas manzanas quieres?")
 total = coste_manzana * int(manzanas)
 print("Total a pagar:" + str(total))
+
 #
 age = int(input("Cuantos años tienes?"))
 if age >= 18:
     print("Eres un adulto independiente")
 else:
     print("Aun sigues siendo un niño")
+    
 #
 año2 = int(input("Dame tu año de nacimiento"))
 año_actual = 2023
@@ -22,6 +24,7 @@ elif total2 == 18:
 else:
     print("Tines " + str(total2) + " años")
     print("Aun sigues siendo un niño")
+    
 #
 numero = 3
 adivinar = int(input("Dime el numero a acertar: "))
@@ -31,8 +34,34 @@ while adivinar != numero:
     adivinar = int(input("Dime el numero a acertar: "))
 
 print("Has adivinado el numero!!  enhorabuena")
+
 #
 for i in range(5):
     if(i == 2):
         break
     print("i is: ", i)
+    
+#
+contador = int(input("Dime un numero: "))
+
+for i in range(100):
+    if(i == contador):
+        break
+    print("i is: ", i)
+
+#
+paises = ["España", "Paises Bajos", "EEUU", "Francia", "Portugal"]
+print(paises[2])
+print(len(paises))
+del paises[1]
+print(paises)
+
+#
+paises = ["España", "Paises Bajos", "EEUU", "Francia", "Portugal"]
+print(paises[2])
+print(len(paises))
+del paises[1]
+print(paises)
+paises.append("Spain")
+paises.insert(3, "Italia")
+print(paises)
